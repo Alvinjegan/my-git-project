5c761c53706de0585e1381a9989529890ee66c8a
Alvin Jegan <alvinjegan.jegan1@gmail.com>
2025-11-13 18:32:08 +0300

    Fix critical bug in script1

--- script1.sh
+++ script1.sh
@@ -2,5 +2,6 @@
 echo "Hello from script 1"
 echo "This is the main script"
 echo "Adding logging functionality"
-echo "[LOG] Script started at \11/13/2025 17:28:09"
+echo "[LOG] Script started at \11/13/2025 18:31:33"
 echo "[LOG] Processing..."
+echo "[BUGFIX] Fixed critical issue"
