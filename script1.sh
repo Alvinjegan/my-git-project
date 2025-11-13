6e84581470cc4edd4082b08f98e9a6f1cf5c0e1f
Alvin Jegan <alvinjegan.jegan1@gmail.com>
2025-11-13 17:29:36 +0300

    Add more logging details

--- script1.sh
+++ script1.sh
@@ -2,4 +2,5 @@
 echo "Hello from script 1"
 echo "This is the main script"
 echo "Adding logging functionality"
-echo "[LOG] Script started at \11/13/2025 17:24:29"
+echo "[LOG] Script started at \11/13/2025 17:28:09"
+echo "[LOG] Processing..."
